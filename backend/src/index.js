45dda5b61e20ab4055ececba5caa0b3ea6b05fc3
Tiago Souza <caladots@outlook.com>
2021-02-22 06:59:10 -0300

    ajustes de delib

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const router = require('./config/routes');
 const cors = require('cors');
@@ -5,6 +6,11 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '../../frontend/build')));
 app.use(router);
 
 app.listen(3333);
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../frontend/build/index.html'));
+});
