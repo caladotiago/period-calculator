ccf6bba2669abd1210d5b6b570cab4929e145c92
Tiago Souza <caladots@outlook.com>
2021-02-22 07:16:47 -0300

    ajustes de delib

--- backend/src/index.js
+++ backend/src/index.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, '../../frontend/build')));
 app.use(router);
 
-app.listen(3333);
+app.listen(process.env.PORT || 3333);
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../../frontend/build/index.html'));
